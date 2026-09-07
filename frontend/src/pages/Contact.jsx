113ffb773b2c8bb726d87917b2b19ac862fd5ea2
instashoutouts2101 <instashoutouts2101@gmail.com>
2026-09-07 11:21:58 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
             setSubmitting(true);
             await window.emailjs.send(
                 "service_23qyqtk",
-                "template_a101eje",
+                "template_vtr7ctj",
                 {
                     name: form.name.trim(),
                     email: form.email.trim(),
